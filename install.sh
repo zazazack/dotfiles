29eeae2e4166cf22656c6d641a72696edd884745
zazazack <wilsonze@gmail.com>
2017-01-05 12:27:20 -0600

    added `--recursive` flag to clone command.

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ FILE_NAME='dotfiles'
 CLONE_URL='https://github.com/zazazack/dotfiles.git'
 
 # clone from github in the users home directory
-echo "changing to $HOME"
+echo "---- changing to $HOME ----"
 cd $HOME
 echo "---- Cloning $CLONE_URL to $HOME ----"
 # NOTE: `--recursive` option initializes and updates the submodule(s) as well
 # source: https://git-scm.com/book/en/v2/Git-Tools-Submodules#Cloning-a-Project-with-Submodules
 git clone --recursive $CLONE_URL $FILE_NAME
-echo "changing to $HOME/$FILENAME"
+echo "---- changing to $HOME/$FILENAME ----"
 cd $FILE_NAME
-
+echo "---- Installing awesome vimrc"
+ln -s $HOME/$FILE_NAME/vimrc $HOME/.vim_runtime
+sh $HOME/$FILE_NAME/vimrc/install_awesome_vimrc.sh
 echo -e '---- Linking files to $HOME ----'
-# version below will NOT overwrite existing files w/ the same name #
-
+# version below will NOT overwrite existing files w/ the same name
 find $PWD -maxdepth 1 -type f -not -iname install.sh -not -iname README.md \
 -not -iname .gitmodules -not -iname .gitignore -exec ln -s '{}' $HOME \;
-
 echo '---- Installation completed successfully ----'
 echo '---- Please exit the shell and RESTART your Terminal ----'
 
@@ -30,3 +30,4 @@ echo '---- Please exit the shell and RESTART your Terminal ----'
 # TODO: exclude `git` files from symlinking to ~/
 # TODO: exclude `README.md` from symlinking to ~/
 # TODO: consider moving files that need to be symlinkd to $HOME to a single directory to simplify the symlinking process and ensure that files that shouldn't be symlinked to $HOME are not.
+# TODO: include a line to install vimrc submodule
