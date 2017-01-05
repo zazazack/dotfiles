6932948632b62eafa1bb4bb8253f0b90ecf93d65
zazazack <wilsonze@gmail.com>
2017-01-05 11:07:09 -0600

    updated `install.sh` to use --recursive option during clone

--- install.sh
+++ install.sh
@@ -10,13 +10,17 @@ CLONE_URL='https://github.com/zazazack/dotfiles.git'
 echo "changing to $HOME"
 cd $HOME
 echo "---- Cloning $CLONE_URL to $HOME ----"
-git clone $CLONE_URL $FILE_NAME
+# NOTE: `--recursive` option initializes and updates the submodule(s) as well
+# source: https://git-scm.com/book/en/v2/Git-Tools-Submodules#Cloning-a-Project-with-Submodules
+git clone --recursive $CLONE_URL $FILE_NAME
 echo "changing to $HOME/$FILENAME"
 cd $FILE_NAME
 
 echo -e '---- Linking files to $HOME ----'
 # version below will NOT overwrite existing files w/ the same name #
-find $PWD -maxdepth 1 -type f -exec ln -sv {} $HOME \;
+
+find $PWD -maxdepth 1 -type f -not -iname install.sh -not -iname README.md \
+-not -iname .gitmodules -not -iname .gitignore -exec ln -s '{}' $HOME \;
 
 echo '---- Installation completed successfully ----'
 echo '---- Please exit the shell and RESTART your Terminal ----'
@@ -25,3 +29,4 @@ echo '---- Please exit the shell and RESTART your Terminal ----'
 # TODO: error check after file backup, and `source`
 # TODO: exclude `git` files from symlinking to ~/
 # TODO: exclude `README.md` from symlinking to ~/
+# TODO: consider moving files that need to be symlinkd to $HOME to a single directory to simplify the symlinking process and ensure that files that shouldn't be symlinked to $HOME are not.
